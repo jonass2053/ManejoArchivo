331ab6650884d8cb88cdd453e042b746cddc4f66
Jonas Diaz Duran <107387650+jonass2053@users.noreply.github.com>
2022-09-08 04:00:31 +0200

    Add files via upload

---
+++ ManejadorDeArchivos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace poo
+{
+    public class ManejadorDeArchivos
+    {
+
+        public static void GuardarArchivo(string nombre, Array arregloString)
+        {
+            FileStream stream = new FileStream(nombre, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter write = new StreamWriter(stream); 
+            
+            foreach(var arreglo in arregloString)
+            {
+                write.WriteLine(arreglo);
+            }
+            write.Close();
+            Console.WriteLine("Se ha Guardado un Archivo correctamente... Presione cualquier tecla para terminar el programa.");
+          
+        }
+
+
+        public static void CopiaArchivo(string RutaEntrada,string RutaSalida)
+        {
+            FileStream stream = new FileStream(RutaEntrada, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            List<string> Listado = new List<string>();  
+         
+
+            while (reader.Peek()>-1)
+            {
+
+
+                Listado.Add(reader.ReadLine());
+                
+              
+            }
+           
+            reader.Close();
+
+            GuardarArchivo(RutaSalida, Listado.ToArray());
+
+            Console.WriteLine("Se ha Creado una copia de  un Archivo correctamente... Presione cualquier tecla para terminar el programa.");
+
+
+        }
+
+
+    }
+}
---
+++ TestManejador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace poo
+{
+    public class TestManejador
+    {
+
+        static void  Main(string[] args)
+        {
+
+
+            //string NombreArchivo = "c:\\io\\file.txt";
+            //FileStream strean = new FileStream(NombreArchivo, FileMode.Open, FileAccess.Read);
+            //StreamReader reader =new StreamReader(strean);
+            //while(reader.Read()>-1)
+            //{
+            //    Console.WriteLine(reader.ReadLine()); 
+            //}
+            //reader.Close();
+
+
+            //string NombreArchivo = "c:\\io\\archivosalida.txt";
+            //FileStream stream = new FileStream(NombreArchivo, FileMode.Append, FileAccess.Write);
+            //StreamWriter write = new StreamWriter(stream);
+            //write.WriteLine("Este documento funsiona?");
+            //write.Close();
+            string Ruta = "c:\\io\\"; 
+            string NombreArchivo = "myfamily.txt";
+            string NombreCompleto = Ruta + NombreArchivo;
+            string[] contenido = {"Jonas Dias Duran", "Dianelis Nivar Matos", "Alaia Silena Diaz Nivar"};
+            ManejadorDeArchivos.GuardarArchivo(NombreCompleto, contenido);
+            string NombreCompletoCopia = Ruta +"Copia"+NombreArchivo;
+            ManejadorDeArchivos.CopiaArchivo(NombreCompleto,  NombreCompletoCopia);
+            
+        }
+
+    }
+}
